d97bb98e34dea246542593c076ae2cd63e8c196e
genbuos <swiftypython18@gmail.com>
2024-09-28 11:06:52 -0400

    insert method created (not yet tested)

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -11,4 +11,7 @@ public class Node {
         right = null;
         left = null;
     }
+
+
+
 }
--- src/main/java/TreeOneZero.java
+++ src/main/java/TreeOneZero.java
@@ -1,8 +1,38 @@
+import java.util.Objects;
+
 /**
  * Created by kristofer on 7/13/20.
  */
 public class TreeOneZero {
     Node root;
 
+    private Node addRecursive(Node current, Integer value){
+        Node newNode = new Node(value);
+       if(root == null){
+           root = newNode;
+           return root;
+       }
+       current = root;
+       while (true){
+           if(newNode.value < current.value){
+               if(current.left == null){
+                   current.left = newNode;
+                   return root;
+               }
+               current = current.left;
+           } else if(value > current.value){
+               if(current.right == null){
+                   current.right = newNode;
+                   return root;
+               }
+               current = current.right;
+           } else {
+               return current;
+           }
+       }
+    }
 
+    public void add(Integer value){
+        this.root = addRecursive(root, value);
+    }
 }
